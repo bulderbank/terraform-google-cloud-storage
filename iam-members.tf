ba0a826abfddf8957e95da14ac5eb0ba029247e1
Fredrick Myrvoll <fredrick.myrvoll@bulderbank.no>
2021-02-24 15:43:35 +0100

    gcs: add legacy reader permissions to read list (#8)

--- iam-members.tf
+++ iam-members.tf
@@ -1,8 +1,18 @@
-resource "google_storage_bucket_iam_member" "iam_read_members" {
+resource "google_storage_bucket_iam_member" "iam_object_viewer" {
   for_each = toset(var.iam_read_members)
 
   bucket = google_storage_bucket.bucket.name
   role   = "roles/storage.objectViewer"
+  member = each.key
+
+  depends_on = [google_storage_bucket.bucket]
+}
+
+resource "google_storage_bucket_iam_member" "iam_legacy_bucket_reader" {
+  for_each = toset(var.iam_read_members)
+
+  bucket = google_storage_bucket.bucket.name
+  role   = "roles/storage.legacyBucketReader"
   member = each.key
 
   depends_on = [google_storage_bucket.bucket]
